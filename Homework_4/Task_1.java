4f90a4d922175769406ea7cd788b3dd49c5188d2
Alexiker99 <alexiker99@gmail.com>
2023-04-26 12:53:15 +0400

    Добавлено Задание №1

---
+++ Homework_4/Task_1.java
@@ -0,0 +1,8 @@
+/* Пусть дан LinkedList с несколькими элементами. 
+Реализуйте метод(не void),который вернет “перевернутый” список.
+*/
+package JavaCourse.Homework_4;
+
+public class Task_1 {
+    
+}
